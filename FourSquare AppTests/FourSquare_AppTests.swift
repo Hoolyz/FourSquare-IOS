0e37754c1d95333dfcb1faca8f7a7f66f9b1bab5
Olli Sirviö <hoolyz@gmail.com>
2018-04-04 23:28:09 +0300

    Initial Commit

---
+++ FourSquare AppTests/FourSquare_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  FourSquare_AppTests.swift
+//  FourSquare AppTests
+//
+//  Created by Olli Sirviö on 04/04/2018.
+//  Copyright © 2018 Olli Sirviö. All rights reserved.
+//
+
+import XCTest
+@testable import FourSquare_App
+
+class FourSquare_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
